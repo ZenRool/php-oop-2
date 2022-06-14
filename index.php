b9005c45b624aecdc3a41f2898058e609cf53e4a
Zenrool <lorenzopapperini@hotmail.it>
2022-06-14 18:48:12 +0200

    Scaffolding

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Titolo -->
+    <title>E-commerce For Animals</title>
+</head>
+<body>
+    <?php
+    require_once "./classes/Product.php";
+    ?>
+</body>
+</html>
